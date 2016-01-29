831d92f19c4483d779f6d9f55525df79e9725c86
sertrimur <sertrimur@gmail.com>
2016-01-29 11:20:53 +0100

    Version final del recolecctor de tweets

--- Analytics/AnalyticsAPP/etc/schedule.js
+++ Analytics/AnalyticsAPP/etc/schedule.js
@@ -4,7 +4,6 @@ var cron = require('node-schedule');
 var ProcessPopularity = require('../etc/processPopularity');
 
 var CollectTweets = require('../etc/collectTweets');
-CollectTweets();
 
 cron.scheduleJob(config.scheduleProcessPopularity.cronExp, ProcessPopularity);
 cron.scheduleJob(config.scheduleProcessCollect.cronTwe, CollectTweets);
